98226fea319ceb9b683631587a313c998ddcb749
mcpe500 <68177813+mcpe500@users.noreply.github.com>
2023-06-07 19:58:25 +0700

    harusnya camera udah bener haruse? coba en

--- src/game/level/Level1.java
+++ src/game/level/Level1.java
@@ -10,15 +10,18 @@ public class Level1 extends PApplet {
     private Tile[][] map;
     private PApplet parent;
     private Player player;
+    private int width, height;
 
     public Level1(PApplet parent) {
+        width = 1280;
+        height = 720;
         this.parent = parent;
         player = new Player(170, 170, 5, 100, 10, 16, 16);
     }
 
     @Override
     public void settings() {
-        size(1280, 720);
+        size(width, height);
     }
 
     public void setup() {
@@ -28,18 +31,18 @@ public class Level1 extends PApplet {
 
     public void draw() {
         // Calculate the camera position to center the player on the screen
-        float cameraX = player.getX() / 2;
-        float cameraY = player.getY() / 2;
+        float cameraX = player.getX() - width / 2;
+        float cameraY = player.getY() - height / 2;
 
         // Limit the camera position to stay within the map boundaries
-        cameraX = constrain(cameraX, 0, map[0].length * 32 - width);
-        cameraY = constrain(cameraY, 0, map.length * 32 - height);
+        cameraX = constrain(cameraX, 50, map[0].length * 32 + 50);
+        cameraY = constrain(cameraY, 50, map.length * 32 + 50);
 
         // Apply camera translation
         pushMatrix();
         translate(-cameraX, -cameraY);
 
-        background(255);
+        background(0);
         for (int i = 0; i < this.map.length; i++) {
             for (int j = 0; j < this.map[i].length; j++) {
                 if (this.map[i][j] != null) {
